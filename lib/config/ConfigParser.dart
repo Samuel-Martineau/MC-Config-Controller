b34cfba022a21d0795bd18ef4a849ff08526630f
Samuel-Martineau <samumartineau@gmail.com>
2020-08-05 21:12:49 -0400

    Fixed Stack Overflow!

--- lib/config/ConfigParser.dart
+++ lib/config/ConfigParser.dart
@@ -1,16 +1,25 @@
 import 'dart:convert';
 
 import 'package:liquid_engine/liquid_engine.dart' as liquid_engine;
+import 'package:quiver/iterables.dart';
+import 'package:uuid/uuid.dart';
 import 'package:yaml/yaml.dart';
 
 class ConfigParser {
   static String parseVars(String content, Map variables) {
     final context = liquid_engine.Context.create();
     context.variables = variables;
-    final template = liquid_engine.Template.parse(
-        context, liquid_engine.Source.fromString(content));
-    return template.render(context);
-    // return Template(content).renderString(variables);
+    final uuid = Uuid().v1();
+    return partition(content.split('\n'), 50)
+        .map((lines) {
+          final chunk =
+              ['chunk_start_$uuid', ...lines, 'chunk_end_$uuid'].join('\n');
+          final template = liquid_engine.Template.parse(
+              context, liquid_engine.Source.fromString(chunk));
+          return template.render(context);
+        })
+        .join('\n')
+        .replaceAll(RegExp('chunk_(start|end)_$uuid(\n)?'), '');
   }
 
   static Map<dynamic, dynamic> parseYAML(String content) {
